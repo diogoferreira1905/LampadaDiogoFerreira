e6bd0d28163efee2d64078b048e91bdaf77ddd44
diogoferreira1905 <113584514+diogoferreira1905@users.noreply.github.com>
2022-09-16 13:19:54 +0100

    Add files via upload

---
+++ JAVASCRIPT EXERCÍCIO 1 EVENTOS/carrega_img.js
@@ -0,0 +1,7 @@
+function acendeLampada(){
+    document.getElementById("myImage").src="./img/lampadaAcessa.jpg";
+}
+
+function apagaLampada() {
+    document.getElementById("myImage").src="./img/lampadaApagada.jpg";
+}
